fa302ef800c03fb03826322d6a19e9d91b3f8c7c
Bruno Jouhier <bjouhier@gmail.com>
2017-05-15 14:30:54 +0200

    DRY pass on unit tests

--- test/basic-test.ts
+++ test/basic-test.ts
@@ -1,6 +1,6 @@
 import { wait, run } from 'f-promise';
 import express = require('express');
-import { Application, Router, Request, Response, NextFunction } from 'express';
+import { Express, IRouterHandler, Application, Router, Request, Response, NextFunction } from '../src';
 import fexpress = require('../src');
 import request = require('supertest');
 
@@ -9,210 +9,71 @@ function delay(x: string) {
     return x;
 }
 
-describe('app.use', function () {
-    it('should work with f-express', function (done) {
-        const app = fexpress();
-        app.use(function (req: Request, res: Response) {
-            res.send(delay("hello"));
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-    it('should fail with express', function (done) {
-        const app = express();
-        app.use(function (req: Request, res: Response) {
-            try {
+function test(name: string, fn: (app: Express) => (body: express.RequestHandler) => any) {
+    describe(name, function () {
+        it('should work with f-express', function (done) {
+            const app = fexpress();
+            fn(app)((req, res) => {
                 res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
+            });
+            request(app)
+                .get('/')
+                .expect(200, 'hello', done);
         });
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
+
+        it('should fail with express', function (done) {
+            const app = express();
+            fn(app)((req, res) => {
+                try {
+                    res.send(delay("hello"));
+                } catch (ex) {
+                    res.status(500).send(ex.message);
+                }
+            });
+            request(app)
+                .get('/')
+                .expect(500, /yield\(\) called with no fiber/, done);
+        });
     });
+}
+
+test('app.use', app => app.use.bind(app));
+test('app.get', app => (handler: express.RequestHandler) => app.get('/', handler));
+
+test('router.get', app => {
+    const router: Router = express.Router();
+    app.use(router);
+    return (handler: express.RequestHandler) => router.get('/', handler);
 });
 
-describe('app.get', function () {
-    it('should work with f-express', function (done) {
-        const app = fexpress();
-        app.get('/', function (req: Request, res: Response) {
-            res.send(delay("hello"));
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-    it('should fail with express', function (done) {
-        const app = express();
-        app.get('/', function (req: Request, res: Response) {
-            try {
-                res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
-        });
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
-    });
+test('simple middleware chain', app => {
+    return (handler: express.RequestHandler) => {
+        app.get('/', (req, res, next) => next(), handler);
+    }
 });
 
-describe('router.get', function () {
+test('middleware chain with one calling next inside a promise resolve', app => {
+    return (handler: express.RequestHandler) => {
+        app.get('/', function (req, res, next) {
+            Promise.resolve().then(next);
+        }, handler);
+    }
+});
+
+describe('error handler middleware', () => {
     it('should work with f-express', function (done) {
         const app: Application = fexpress();
-        const router: Router = express.Router();
-        router.get('/', function (req: Request, res: Response) {
-            res.send(delay("hello"));
+
+        app.get('/', function (req, res, next) {
+            next(new Error("testing"));
         });
-        app.use(router);
+
+        app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+            res.status(500).send(`ERR: ${err.message}`);
+        });
+
         request(app)
             .get('/')
-            .expect(200, 'hello', done);
-    });
-
-    it('should fail with express', function (done) {
-        const app: Application = express();
-        const router: Router = express.Router();
-        router.get('/', function (req: Request, res: Response) {
-            try {
-                res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
-        });
-        app.use(router);
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
-    });
-});
-
-describe('simple middleware chain', () => {
-    it('should work with f-express', function (done) {
-        const app = fexpress();
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            next();
-        }, function (req: Request, res: Response) {
-            res.send(delay("hello"));
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-    it('should fail with express', function (done) {
-        const app: Application = express();
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            next();
-        }, function (req: Request, res: Response) {
-            try {
-                res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
-        });
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
-    });
-
-    it('should works with express if encapsulate with run', function (done) {
-        const app: Application = express();
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            next();
-        }, function (req: Request, res: Response) {
-            run(() => {
-                res.send(delay("hello"));
-            }).catch(ex => {
-                res.status(500).send(ex.message);
-            });
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-    it('should works with express if previous handler encapsulate with run', function (done) {
-        const app: Application = express();
-        app.use(function(req: Request, res: Response, next: NextFunction) {
-            run(() => {
-                next();
-            }).catch(next)
-        });
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            next();
-        }, function (req: Request, res: Response) {
-            res.send(delay("hello"));
-
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-});
-
-describe('middleware chain with one calling next inside a promise resolve', () => {
-    it('should work with f-express', function (done) {
-        const app = fexpress();
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            Promise.resolve()
-                .then(function () {
-                    next();
-                });
-        }, function (req: Request, res: Response) {
-            res.send(delay("hello"));
-        });
-        request(app)
-            .get('/')
-            .expect(200, 'hello', done);
-    });
-
-
-    it('should fail with express', function (done) {
-        const app: Application = express();
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            Promise.resolve()
-                .then(function () {
-                    next();
-                });
-        }, function (req: Request, res: Response) {
-            try {
-                res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
-        });
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
-    });
-
-    it('should fail with express even if previous handler encapsulate with run', function (done) {
-        const app: Application = express();
-        app.use(function(req: Request, res: Response, next: NextFunction) {
-            run(() => {
-                next();
-            }).catch(next)
-        });
-        app.get('/', function (req: Request, res: Response, next: NextFunction) {
-            Promise.resolve()
-                .then(function () {
-                    next();
-                });
-        }, function (req: Request, res: Response) {
-            try {
-                res.send(delay("hello"));
-            } catch (ex) {
-                res.status(500).send(ex.message);
-            }
-        });
-        request(app)
-            .get('/')
-            .expect(500, /yield\(\) called with no fiber/, done);
+            .expect(500, 'ERR: testing', done);
     });
 });
